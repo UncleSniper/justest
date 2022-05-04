1f4d590c5fc64d79da3d45d74c727d92191feb85
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2022-05-05 01:05:54 +0200

    Added test testcases to RunAllTests

--- test/org/unclesniper/test/RunAllTests.java
+++ test/org/unclesniper/test/RunAllTests.java
@@ -2,8 +2,32 @@ package org.unclesniper.test;
 
 public class RunAllTests {
 
+	private static final Testable<Void> ALL_TESTS = TestBuilders.testsuite("RunAllTests", RunAllTests::new,
+		TestBuilders.testcase("subtypes", RunAllTests::testSubtypes),
+		TestBuilders.testcase("failure", RunAllTests::testFailure),
+		TestBuilders.testcase("skip", RunAllTests::testSkip)
+	);
+
 	public static void main(String[] args) throws Exception {
-		System.out.println("Hello, world!");
+		PrintStreamTestResultSink sink = new PrintStreamTestResultSink(System.out);
+		sink.setShowSkipped(true);
+		sink.setShowSkippedDetails(false);
+		sink.setShowPassed(true);
+		Testable.run(null, sink, RunAllTests.ALL_TESTS);
+		if(sink.getFailedCases() > 0)
+			System.exit(1);
+	}
+
+	private void testSubtypes(TestContext context) {
+		Assert.<SimpleTestsuite<RunAllTests>, Testable<RunAllTests>>assertSubtype();
+	}
+
+	private void testFailure() {
+		Assert.fail("failing statically");
+	}
+
+	private void testSkip() {
+		Assume.skip("skipping statically");
 	}
 
 }
