5d9f2da1b8717188d696060574fcdb617a417618
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2022-05-05 00:45:58 +0200

    Added Testable.run()

--- src/org/unclesniper/test/Testable.java
+++ src/org/unclesniper/test/Testable.java
@@ -6,4 +6,10 @@ public interface Testable<BaseT> {
 
 	void performTest(TestContext context, BaseT base, TestResultSink sink) throws IOException;
 
+	public static void run(TestContext context, TestResultSink sink, Testable<Void> test) throws IOException {
+		if(test == null)
+			throw new IllegalArgumentException("Test must not be null");
+		test.performTest(context == null ? EmptyTestContext.instance : context, null, sink);
+	}
+
 }
